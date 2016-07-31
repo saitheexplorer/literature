d2cb174594f0e6bdf18c5d0702ba6c372a1492d3
Sai Gunturi <sai@nectarom.com>
2016-07-31 09:56:55 -0500

    Refactor let to const.

--- src/reducers/memory.js
+++ src/reducers/memory.js
@@ -5,21 +5,18 @@ import LiteratureDeck from 'utils/deck';
 import { removePlayerFromPossibleOwners, simplifyPossibleOwners } from 'utils/memory';
 
 export function memoryDeck(state = List(LiteratureDeck), action) {
-  let possibleOwners;
-  let updatedPossibleOwners;
-
   switch (action.type) {
     case Constants.START_GAME:
       return state.map(card => card.set('possibleOwners', Range(1, action.numberOfPlayers + 1).toList().map(String)));
 
     case Constants.TRANSFER_CARD:
-      let transferCardIndex = state.findIndex(card => card.get('card') === action.askedCard);
+      const transferCardIndex = state.findIndex(card => card.get('card') === action.askedCard);
 
       return state.setIn([transferCardIndex, 'owner'], action.askingPlayer).setIn([transferCardIndex, 'possibleOwners'], List());
 
     case Constants.ASK_QUESTION:
-      let askedCardIndex = state.findIndex(card => card.get('id') === action.askedCard);
-      let askedCard = state.get(askedCardIndex);
+      const askedCardIndex = state.findIndex(card => card.get('id') === action.askedCard);
+      const askedCard = state.get(askedCardIndex);
 
       if (askedCard.get('owner')) return state;
 
@@ -28,8 +25,8 @@ export function memoryDeck(state = List(LiteratureDeck), action) {
         .map(simplifyPossibleOwners);
 
     case Constants.DENY_QUESTION:
-      let deniedCardIndex = state.findIndex(card => card.get('id') === action.askedCard);
-      let deniedCard = state.get(deniedCardIndex);
+      const deniedCardIndex = state.findIndex(card => card.get('id') === action.askedCard);
+      const deniedCard = state.get(deniedCardIndex);
 
       if (deniedCard.get('owner')) return state;
 
