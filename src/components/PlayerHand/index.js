5cbb784ff0c0b030f9f0a100dca9b2a679e93fb8
Sai Gunturi <sai@nectarom.com>
2016-07-31 00:18:13 -0500

    Refactor PlayerHand component to stateless function.

--- src/components/PlayerHand/index.js
+++ src/components/PlayerHand/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-export default class PlayerHand extends React.Component {
-  render() {
-    let hand = this.props.hand;
+const PlayerHand = ({ hand, player }) => {
+  if (player !== '1') return <p>Player {player} - {hand.size} cards remaining.</p>;
 
-    if (this.props.player !== '1') return <p>Player {this.props.player} - {hand.size} cards remaining.</p>;
+  const cards = hand
+    .sortBy(card => card.get('id'))
+    .map(card => <li key={card.get('id')}>{card.get('id')}</li>);
 
-    let cards = hand
-      .sortBy(card => card.get('id'))
-      .map(card => <li key={card.get('id')}>{card.get('id')}</li>);
-
-    return (
-      <div>
-        <p>Player {this.props.player} Cards:</p>
-        <ul>{cards}</ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>Player {player} Cards:</p>
+      <ul>{cards}</ul>
+    </div>
+  );
 }
+
+export default PlayerHand;
